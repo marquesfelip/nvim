284756b7025027641e3351d5d006f313ec7584d6
Felipe Marques <felipehost@hotmail.com>
2025-10-01 22:33:19 -0300

    update lspconfig require

--- init.lua
+++ init.lua
@@ -594,7 +594,10 @@ require('lazy').setup({
             -- by the server configuration above. Useful when disabling
             -- certain features of an LSP (for example, turning off formatting for ts_ls)
             server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            require('lspconfig')[server_name].setup(server)
+            -- FIXME: the command below is deprecated
+            -- require('lspconfig')[server_name].setup(server)
+            vim.lsp.config(server_name, server)
+            vim.lsp.enable(server_name)
           end,
         },
       }
@@ -812,7 +815,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight'
+      vim.cmd.colorscheme 'tokyonight-night'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -10,7 +10,7 @@ return {
       options = {
         theme = 'palenight',
         component_separators = { left = '', right = '' },
-        section_separators = { left = '', right = '' },
+        -- section_separators = { left = '', right = '' },
       },
       sections = {
         lualine_x = {
